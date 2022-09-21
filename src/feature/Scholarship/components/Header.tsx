2bda524dff1c4905308fa789d155145fe3375024
Nithin S Varrier <nithin486@hotmail.com>
2022-09-21 19:51:40 +0530

    bugfix: Update logo

--- src/feature/Scholarship/components/Header.tsx
+++ src/feature/Scholarship/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../assets/main-logo-Banner.webp";
+import Logo from "../../../assets/main-logo.svg";
 import classes from "./Header.module.scss";
 function Header() {
   return (
